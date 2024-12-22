b2701f93291f66f828dff0f34aae1ee72d9ca36e
tsaolun <ycl18@live.com>
2024-12-23 01:37:19 +0800

    modified:   src/training.rs

--- src/training.rs
+++ src/training.rs
@@ -1,10 +1,11 @@
-use burn::{prelude::*, train::ClassificationOutput};
+use burn::{prelude::*, tensor::backend::AutodiffBackend, train::{ClassificationOutput, TrainOutput, TrainStep, ValidStep,}};
 use nn::loss::CrossEntropyLossConfig;
 
 use crate::model::Model;
+use crate::data::MnistBatch;
 
 impl<B: Backend> Model<B> {
-    pub fn forward_classifition(
+    pub fn forward_classification(
         &self,
         images: Tensor<B, 3>,
         targets: Tensor<B, 1, Int>,
@@ -14,5 +15,18 @@ impl<B: Backend> Model<B> {
             .init(&output.device())
             .forward(output.clone(), targets.clone());
         ClassificationOutput::new(loss, output, targets)
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, batch: MnistBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
+        let item = self.forward_classification(batch.images, batch.targets);
+        TrainOutput::new(self, item.loss.backward(), item)
+    }
+}
+
+impl<B: Backend> ValidStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, batch: MnistBatch<B>) -> ClassificationOutput<B> {
+        self.forward_classification(batch.images, batch.targets)
     }
 }
